6cc2789d3751ba8face3b765fe757e8bc24c5e9e
KandhimallaNeha <154578875+KandhimallaNeha@users.noreply.github.com>
2024-06-04 16:23:23 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+    def Speak(self):
+        print(f"Hii my name is {self.name} and my age is {self.age}")
+        
+Ammu=Person("NEHA",21)
+Ammu.Speak()
+
+
+
